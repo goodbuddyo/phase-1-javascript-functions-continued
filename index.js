c8e7e1cc0364dc955c8b9bac855893084caa3390
Seano <sean01234@yahoo.com>
2023-03-23 21:19:34 -0400

    nested functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+  return ('This Saturday, I want to ' + activity + '!')
+}
+console.log(saturdayFun());
+
+let mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective(hightlight = "*") {
+  return function (msg = "special",) {
+    return `You are ${hightlight}${msg}${hightlight}!`;
+  };
+  let result = wrapAdjective()
+}
